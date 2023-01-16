d91afec65d00c25954e0a5fa1d41890464b746eb
KingSandy01 <ssatapathy90@gmail.com>
2023-01-16 17:55:49 +0530

    hi-created-char-Addition

--- calculator.cpp
+++ calculator.cpp
@@ -5,9 +5,9 @@ int main(){
     int a, b, c, opt;
     c=0;
 
-    do{
     cout<<"\nChoose among the following Options: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. Exit \n (Choose the option as 1/2/3/4/5 to proceed): ";
     cin>>opt;
+   
     switch(opt){
         case 1: 
             cout<<"\nInput First Value: ";
@@ -16,6 +16,7 @@ int main(){
             cin>>b;
             c=a+b;
             cout<<"\nSum of First and Second value is: "<<c;
+            break;
         case 2:
             cout<<"\nInput First Value: ";
             cin>>a;
@@ -23,6 +24,7 @@ int main(){
             cin>>b;
             c=a-b;
             cout<<"\nSubtracting of Second from First value we get: "<<c;
+            break;
         case 3:
             cout<<"\nInput First Value: ";
             cin>>a;
@@ -30,6 +32,7 @@ int main(){
             cin>>b;
             c=a*b;
             cout<<"\nProduct of First and Second value is: "<<c;
+            break;
         case 4:
             cout<<"\nInput First Value: ";
             cin>>a;
@@ -37,10 +40,12 @@ int main(){
             cin>>b;
             c=a/b;
             cout<<"\nDivision of First by Second value is: "<<c;
+            break;
         case 5:
+            cout<<"You Choose to Exit...";
             break;
     }
-    }while(opt !=5);
+    
 
     return 0;
 
---
+++ hi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char c1= 'H';
+    char c2= 'I';
+    char c3= '!';
+
+    int z = c1 + c2 + c3;
+
+    printf("%i %i %i | %i \n", c1, c2, c3, z );
+
+}
