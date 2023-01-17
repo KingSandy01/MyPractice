fa6f56091c4ed4972ab5dfd6eb8b8ab22c809329
KingSandy01 <ssatapathy90@gmail.com>
2023-01-17 23:50:07 +0530

    helloWorld-v1

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"\n What is your name? \n";
+    cin>>name;
+    cout<<"\n Hello, "<<name;
+
+    return 0;
+    
+}
